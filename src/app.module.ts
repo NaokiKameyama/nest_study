1cef5c5b3122a296c94df951b6800147825ccfc1
Naoki-Kameyama <atukan0930@gmail.com>
2021-06-16 03:07:01 +0900

    mongoDBのURLをコメントに追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose'
   imports: [
     UsersModule,
     MongooseModule.forRoot(
+      // [mongoDB]:https://cloud.mongodb.com/v2/60c8dc8f85fde969977a86ec#clusters
       'mongodb+srv://atukan0930:8GNSzyEN7vNopEc1@cluster0.5fngo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     ),
   ],
